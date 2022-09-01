8a788a42a43d3f366c08488ed62ce148e386ec2a
Mike Rachman <mikerachman1@gmail.com>
2022-09-01 11:57:19 -0400

    added validations to controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
+  validates :password, presence: true, length: { in: 6..14 }
+  validates :email, presence: true, uniqueness: true
+
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
